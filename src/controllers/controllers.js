d6abe6adf097d6e4e75cd7139f55b984f08d6b21
curtyraissa <raissacurty@gmail.com>
2023-05-11 19:25:28 -0300

    estrutura inicial controllers

---
+++ src/controllers/controllers.js
@@ -0,0 +1,89 @@
+export async function inserirJogo(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function listarJogos(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function inserirCliente(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function listarClientes(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function buscarClienteId(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function atualizarCliente(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function listarAlugueis(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function inserirAluguel(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  export async function finalizarAluguel(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+  
+  export async function apagarAluguel(req, res) {
+    try {
+    //   const receitas = await db.query("SELECT * FROM receitas");
+    //   res.send(receitas.rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
