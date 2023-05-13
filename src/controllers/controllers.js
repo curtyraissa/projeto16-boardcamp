d63f51c48389d615b96dbf5762f1a82423bea4eb
curtyraissa <raissacurty@gmail.com>
2023-05-12 23:06:36 -0300

    atualizarCliente

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -113,9 +113,8 @@ export async function atualizarCliente(req, res) {
 
   try {
     const cpfExiste = (
-      await db.query("SELECT * FROM customers WHERE cpf=$1 AND id!=$2", [cpf, id])
-    ).rows;
-    if (cpfExiste) return res.sendStatus(409);
+      await db.query("SELECT * FROM customers WHERE cpf=$1 AND id!=$2", [cpf, id]));
+    if (cpfExiste.rowCount) return res.sendStatus(409);
 
     await db.query(
       `UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5)`,
