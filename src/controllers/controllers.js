d21f2274c5591f407ccb1113dc46a195710b8aa2
curtyraissa <raissacurty@gmail.com>
2023-05-14 00:50:45 -0300

    correcao function listarAlugueis

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -120,7 +120,7 @@ export async function atualizarCliente(req, res) {
 
 export async function listarAlugueis(req, res) {
   try {
-    const resultado = await db.query(` SELECT rentals.*, customers.name AS "nomeCliente", games.name AS nomeJogoFROM rentals
+    const resultado = await db.query(` SELECT rentals.*, customers.name AS "nomeCliente", games.name AS "nomeJogo" FROM rentals
     JOIN games ON rentals."gameId"=games.id
     JOIN customers ON rentals."customersId"=customers.id`);
 
