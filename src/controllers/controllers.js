2a389bb5b0276744799507765b1270dff02bc09e
curtyraissa <raissacurty@gmail.com>
2023-05-14 12:18:59 -0300

    function finalizarAluguel

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -215,6 +215,8 @@ export async function inserirAluguel(req, res) {
 export async function finalizarAluguel(req, res) {
     const {id} = req.params;
   try {
+    const returnDate = new Date().toISOString().slice(0, 10)
+    
     const aluguelExiste = await db.query(`SELECT rentals.*, games."pricePerDay"
     FROM rentals 
     JOIN games ON rentals."gameId" = games.id
@@ -222,6 +224,17 @@ export async function finalizarAluguel(req, res) {
 
     if (aluguelExiste.rows.length == 0) return res.sendStatus(404);
     if(aluguelExiste.rows[0].returnDate !== null) return res.sendStatus(400);
+
+    const { rentDate, daysRented, pricePerDay } = aluguelExiste.rows[0];
+    const atraso = Math.max(Math.floor((Date.now() - (new Date(rentDate)).getTime() - (daysRented * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)), 0);
+    const delayFee = atraso > 0 ? atraso * pricePerDay : null;
+    
+    await db.query(`
+    UPDATE rentals
+    SET "returnDate" = $1, "delayFee" = $2
+    WHERE "id" = $3;
+`, [returnDate, delayFee, id]);
+
 
     res.sendStatus(200)
   } catch (err) {
