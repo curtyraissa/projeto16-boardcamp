7a20ba815226365447ab732b12cc875a9a18234e
curtyraissa <raissacurty@gmail.com>
2023-05-15 17:18:02 -0300

    alteracoes para deploy

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -8,7 +8,6 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-// MODE = "prod"
 // if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
